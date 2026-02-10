712a08d6f1b170e7e1e1b529ac22f4c02cafac47
Don <120154112+zen-da-baker@users.noreply.github.com>
2026-02-09 19:52:19 -0800

    Dynamically display rows for the month test

--- express-app/src/client/dashboard/calendar/calendar-display/displayCalendarDaysGrid.ts
+++ express-app/src/client/dashboard/calendar/calendar-display/displayCalendarDaysGrid.ts
@@ -4,11 +4,20 @@ export function displayCalendarDaysGrid(): void {
 
     let currentDate = new Date();
 
+    let firstDayOfTheMonth = new Date( currentDate );
+
+    firstDayOfTheMonth.setDate( 1 );
+
+    let firstDayPosition = firstDayOfTheMonth.getDay();
+
     let dayIterations = 0;
 
-    let totalIterations: number = 1;
+    let totalCalendarRows: number = 1;
 
-    for ( let outerIterator = 0; outerIterator < totalIterations; outerIterator++ ) {
+    let maxDaysInTheMonth = 31;
+
+    // The number of rows the calendar will have
+    for ( let outerIterator = 0; outerIterator < totalCalendarRows; outerIterator++ ) {
 
         let calendarDaysRowUI = document.createElement("div");
 
@@ -16,6 +25,7 @@ export function displayCalendarDaysGrid(): void {
 
         calendarDaysRowUI.className = "flex calendar-row";
 
+        // The number of columns a row has which is always fixed to 7 days
         for ( let innerIterator = 0; innerIterator < 7; innerIterator++ ) {
 
             let calendarDayUI = document.createElement("div");
@@ -28,7 +38,14 @@ export function displayCalendarDaysGrid(): void {
 
             } 
 
-            if ( dayIterations + 1 === currentDate.getDate() ) {
+            // If the current day in the calendar is of the same date, month, and year, highlight it
+            if ( 
+                dayIterations + 1 === currentDate.getDate() 
+                && 
+                currentDate.getFullYear() === firstDayOfTheMonth.getFullYear()  
+                &&
+                currentDate.getMonth() === firstDayOfTheMonth.getMonth()
+            ) {
 
                 calendarDayUI.className += " current-day";
 
@@ -38,15 +55,32 @@ export function displayCalendarDaysGrid(): void {
 
             calendarDaysRowUI.appendChild( calendarDayUI );
 
-            dayIterations++;
+            // If the first day is equal to the current display position for the first week row, start the day count
+            if ( firstDayPosition === innerIterator && outerIterator === 1 ) {
+
+                dayIterations++;
+        
+            }
+
+            if ( dayIterations > 0 && dayIterations <= maxDaysInTheMonth ) {
+
+                dayIterations++;
+
+            }
 
         }
 
-        if ( totalIterations < 6 ) {
+        // The total number of rows is determined by the number of days after the first row is established
+        if ( dayIterations < maxDaysInTheMonth ) {
 
-            totalIterations++;
+            // Add a new row to the count while there are still days to cycle through
+            totalCalendarRows += 1;
 
         }
+
+        console.log( totalCalendarRows );
+
+        console.log( dayIterations );
 
         calendarDaysGridUI.appendChild( calendarDaysRowUI );
 
