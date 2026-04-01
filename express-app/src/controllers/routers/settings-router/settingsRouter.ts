499050870a0086f73c040544c7afbff223ef8b14
Don <120154112+zen-da-baker@users.noreply.github.com>
2026-03-31 21:26:52 -0700

    Upload all local journal entries created

--- express-app/src/controllers/routers/settings-router/settingsRouter.ts
+++ express-app/src/controllers/routers/settings-router/settingsRouter.ts
@@ -4,16 +4,18 @@ import express from "express";
 // Import controller functions
 import { changePasswordHandler } from "../../put-settings/changePasswordHandler.js";
 import { getOneEntry } from "../../../database/journal-entry-helpers/getOneEntry.js";
+import { getAllEntries } from "../../../database/journal-entry-helpers/getAllEntries.js";
+import { updateAllEntries } from "../../../database/journal-entry-helpers/updateAllEntries.js";
 
 const settingsRouter = express.Router();
 
 // Get all journal entries
-// settingsRouter.get( "/all-journal-entries", );
+settingsRouter.get( "/all-entries", getAllEntries );
 
 settingsRouter.get( "/entry", getOneEntry );
 
 // Upload all journal entries as an update
-// settingsRouter.put( "/all-journal-entries", );
+settingsRouter.put( "/all-entries", updateAllEntries );
 
 // settingsRouter.put( "/entry", );
 
--- express-app/src/database/journal-entry-helpers/updateAllEntries.ts
+++ express-app/src/database/journal-entry-helpers/updateAllEntries.ts
@@ -1,3 +1,16 @@
+// Import data models
+import { Token } from "../../client/models/Token.js";
+import { User } from "../../controllers/server-models/User.js";
+
+// Import helper functions
+import { validateUserStrings } from "../../helpers/validateUserStrings.js";
+import { validateUniqueUsername } from "../validateUniqueUsername.js";
+import { findUser } from "../findUser.js";
+import { verifyUserToken } from "../verifyUserToken.js";
+
+// Import database
+import { database } from "../../server.js";
+
 export async function updateAllEntries( request: any, response: any ) {
 
     const body = request.body;
@@ -10,6 +23,55 @@ export async function updateAllEntries( request: any, response: any ) {
 
     }
 
-    // if ( body. )
+    if ( !body.entriesList || !body.token ) {
+
+        return response.status( 400 ).json({
+            msg: "Needed details are missing from this request."
+        })
+
+    }
+
+    // The token is parsed
+    const token: Token = JSON.parse( body.token );
+
+    // The token string is parsed or else an error is thrown if the fields do not exist
+    token.username = validateUserStrings( token.username );
+    token.id = validateUserStrings( token.id );
+    token.expirationDate = validateUserStrings( token.expirationDate );
+
+    // Validate the user token
+    let nonExistantUser = await validateUniqueUsername( token.username );
+
+    if ( nonExistantUser ) {
+
+        return response.status( 400 ).json({
+            msg: "This user does not exist."
+        })
+
+    }
+
+    let user: User = await findUser( token.username );
+
+    let verifiedUser = verifyUserToken( user, token );
+
+    if ( !verifiedUser ) {
+
+        return response.status( 400 ).json({
+            msg: "The user could not be verified."
+        })
+
+    }
+
+    let entriesList = body.entriesList;
+
+    let username = token.username;
+
+    const userCollection = database.collection( username );
+
+    userCollection.insertMany( entriesList );
+    
+    return response.status( 200 ).json({
+        msg: "Journal entries added to the website succesfully."
+    })
 
 }
